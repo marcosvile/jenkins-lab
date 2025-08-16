e7b3d9a9f8fc2f363b0aab87197714aa25efc1bf
Marcos Vilela <marcosvilelateixeira@gmail.com>
2025-08-15 23:48:38 -0300

    adiciona script provision.sh para instalação do Jenkins e Docker

---
+++ provision.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -euo pipefail
+
+set -a
+set +a
+
+main() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "Run as root."
+        exit 1
+    fi
+
+# Instalação do Jenkins
+sudo apt update
+sudo apt install wget unzip openjdk-17-jre -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-archive-keyring.gpg] \
+  https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+
+# Instalação do docker e docker compose
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker "$USER"
+
+}
+
+main $@
